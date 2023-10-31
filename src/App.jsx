53395de0ed41e5586528fcc83b611588d0922107
Giovanny Vargas <yovaz05@gmail.com>
2023-10-31 18:49:11 -0500

    clase 31-10-20236

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Dashboard from "./pages/Dashboard";
+import UsersPages from "./pages/UsersPages";
+import UserPage from "./pages/UserPage";
+import NotFound from "./pages/NotFound";
+
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="" element="" />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/users" element={<UsersPages />} />
+          <Route path="/user" element={<UserPage />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/components/Menu.jsx
@@ -0,0 +1,19 @@
+const Menu = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/dashboard">Home</a>
+        </li>
+        <li>
+          <a href="/users">Home</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Menu;
